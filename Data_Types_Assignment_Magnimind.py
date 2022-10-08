85d1987c497df452386a4dfb61e6ee822e23c73c
sandyl3188 <81432798+sandyl3188@users.noreply.github.com>
2022-10-07 20:11:59 -0700

    Add files via upload
    Uploaded from Macbook files via Jupyter host

---
+++ Data_Types_Assignment_Magnimind.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+capital = 1000
+daily_growth = .12
+days = 7
+
+
+# In[6]:
+
+
+1000*.12
+
+
+# In[7]:
+
+
+120*7
+
+
+# In[8]:
+
+
+total_growth= 1840
+
+
+# In[18]:
+
+
+C = (5/9)
+F = 98
+
+
+# In[19]:
+
+
+C * F
+
+
+# In[24]:
+
+
+CF= 54.44
+CF - 32
+
+
+# In[25]:
+
+
+Celcius_is= 22.43
+
+
+# In[23]:
+
+
+14/3
+
+
+# In[26]:
+
+
+first_side_lenth = 6
+second_line_lenth = 8
+
+
+# In[27]:
+
+
+first_side_lenth*second_line_lenth
+
+
+# In[28]:
+
+
+hypotenuse_length= 48
+
+
+# In[ ]:
+
+
+
+
